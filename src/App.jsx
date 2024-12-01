4d4ae1f37d177c0656a566956bd71760e3526db1
Jintae Song <w.jinttao@gmail.com>
2024-12-01 11:08:06 +0900

    Test: apply Intercom

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Intercom from '@intercom/messenger-js-sdk'
 
 function App() {
   const [count, setCount] = useState(0)
+  const user = {
+    id: "TEST_ID",
+    name: "TEST_NAME",
+    email: "TEST_EMAIL",
+    createdAt: "2024-12-01",
+  }
+  const i = Intercom({ // 화면에 그리지도 않았는데, Intercome 함수 호출만으로 어떻게 UI에 표시되는지 체크.
+    app_id: import.meta.env.VITE_INTERCOM_APP_ID,
+    user_id: user.id, // IMPORTANT: Replace "user.id" with the variable you use to capture the user's ID
+    name: user.name, // IMPORTANT: Replace "user.name" with the variable you use to capture the user's name
+    email: user.email, // IMPORTANT: Replace "user.email" with the variable you use to capture the user's email
+    created_at: user.createdAt, // IMPORTANT: Replace "user.createdAt" with the variable you use to capture the user's sign-up date in a Unix timestamp (in seconds) e.g. 1704067200
+  });
+  console.log("i", i); // undefined
 
   return (
     <>
